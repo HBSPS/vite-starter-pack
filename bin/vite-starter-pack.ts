60739fbdfacb15c5d2248cde46bdd1d84f4283c9
HBSPS <wiyeong79@naver.com>
2024-07-04 21:34:14 +0900

    feat: add questions

--- bin/vite-starter-pack.ts
+++ bin/vite-starter-pack.ts
@@ -1,6 +1,99 @@
 #!/usr/bin/env node
 
-import { input } from '@inquirer/prompts';
+import { confirm, select } from '@inquirer/prompts';
 
-const answer = await input({ message: 'Enter your name' });
-console.log(answer);
+try {
+  const usingCreateVite = await confirm({
+    message: 'Did you use create vite for your project? (or does your project already have eslint?)',
+  });
+
+  if (!usingCreateVite) {
+    const installEslint = await confirm({
+      message: 'Do you want to install eslint?',
+    });
+
+    if (installEslint) console.log('install eslint');
+  }
+
+  const installPrettier = await confirm({
+    message: 'Do you want to install prettier?',
+  });
+
+  if (installPrettier) console.log('install prettier');
+
+  const installLintStaged = await confirm({
+    message: 'Do you want to install lint-staged?',
+  });
+
+  if (installLintStaged) console.log('install lint-staged');
+
+  const installHusky = await confirm({
+    message: 'Do you want to install husky?',
+  });
+
+  if (installHusky) console.log('install husky');
+
+  const installStyling = await select({
+    message: 'Do you want to install styling library?',
+    choices: [
+      {
+        name: '(no)',
+        value: 'no',
+      },
+      {
+        name: 'tailwind',
+        value: 'tailwind',
+      },
+      {
+        name: 'emotion',
+        value: 'emotion',
+      },
+    ],
+  });
+
+  switch (installStyling) {
+    case 'no':
+      console.log('no');
+      break;
+
+    case 'tailwind':
+      console.log('install tailwind');
+      break;
+
+    case 'emotion':
+      console.log('install emotion');
+      break;
+  }
+
+  const installTesting = await select({
+    message: 'Do you want to install testing library?',
+    choices: [
+      {
+        name: '(no)',
+        value: 'no',
+      },
+      {
+        name: 'vitest',
+        value: 'vitest',
+      },
+      {
+        name: 'jest',
+        value: 'jest',
+      },
+    ],
+  });
+
+  switch (installTesting) {
+    case 'no':
+      console.log('no');
+      break;
+
+    case 'vitest':
+      console.log('install vitest');
+      break;
+
+    case 'jest':
+      console.log('install jest');
+      break;
+  }
+} catch {}
